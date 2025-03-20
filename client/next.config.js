bf2a0611b93fdaeadc8010589d42a007e6bf58bc
Prateek <prateekmahapatra1926@gmail.com>
2025-03-20 20:16:00 +0530

    added to ignore eslint

---
+++ client/next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+    eslint: {
+      // Warning: This allows production builds to succeed even if there are ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+  };
+  
