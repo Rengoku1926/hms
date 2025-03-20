5a11c32381cbd91dcbfedc9b40fc5423caed9418
Prateek <prateekmahapatra1926@gmail.com>
2025-03-20 21:53:17 +0530

    updated code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
-import cors from "cors";
+import cors from "cors"
 import serverless from "serverless-http";
 
 dotenv.config();
 
+
 const app = express();
-app.use(cors());
+app.use(cors())
+
+// Middleware to parse JSON
 app.use(express.json());
+
+// Mount all routes under the /api prefix
 app.use("/api", routes);
 
-// Only start the server if not running in a serverless environment
-if (!process.env.VERCEL_ENV) {
-  const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
-  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
-}
-
-export const handler = serverless(app);
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
